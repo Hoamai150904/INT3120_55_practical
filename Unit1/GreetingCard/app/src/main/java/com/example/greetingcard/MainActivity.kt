a441c4960f46e5652e50ba4ad2ec99a53f61dfd8
Hoamai150904 <hoamai1509@gmail.com>
2024-09-12 00:04:37 +0700

    pathway 2
    Change little bit code

--- Unit1/GreetingCard/app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ Unit1/GreetingCard/app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GreetingCardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Mai",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
