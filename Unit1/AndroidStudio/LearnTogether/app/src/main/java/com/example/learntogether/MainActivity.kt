8a9e8e7a2445fe121b562727b009262080339ab0
Hoamai150904 <hoamai1509@gmail.com>
2024-09-13 12:52:12 +0700

    Learntogether.kt
    đã xog

--- Unit1/AndroidStudio/LearnTogether/app/src/main/java/com/example/learntogether/MainActivity.kt
+++ Unit1/AndroidStudio/LearnTogether/app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -77,15 +77,6 @@ private fun Card(
     }
 }
 
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun ComposeArticleAppPreview() {
